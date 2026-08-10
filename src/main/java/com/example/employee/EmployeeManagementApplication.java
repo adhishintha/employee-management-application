e52206a771ed6bc0558707aa99f247b60144250b
Danesh-S-Durgada <daneshsd2002@gmail.com>
2026-08-10 18:39:37 +0530

    My initial commit

---
+++ src/main/java/com/example/employee/EmployeeManagementApplication.java
@@ -0,0 +1,17 @@
+// package main.java.com.example.employee;
+
+// public class EmployeeManagementApplication {
+    
+// }
+package com.example.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManagementApplication.class, args);
+    }
+}
